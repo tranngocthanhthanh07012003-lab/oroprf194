03e01aae63b58ee7b19ae6371609670c989b5e40
tranngocthanhthanh07012003-lab <tranngocthanhthanh07012003@gmail.com>
2025-08-16 15:45:20 +0700

    Update main.js

--- main.js
+++ main.js
@@ -55,7 +55,7 @@ const MNEMONICS = [
 console.log(`✅ Đã load ${MNEMONICS.length} ví trực tiếp từ code`);
 
 const CONFIG = {
-    rpcEndpoint: "https://testnet-rpc.zigchain.com",
+    rpcEndpoint: "https://public-zigchain-testnet-rpc.numia.xyz",
     chainId: "zig-test-2",
     zigDenom: "uzig",
     oroDenom: "coin.zig10rfjm85jmzfhravjwpq3hcdz8ngxg7lxd0drkr.uoro",
